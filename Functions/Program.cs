21e467505fbe90198dffb94f95eda6a79f370a44
ThilNguyen <tnteee.nguyen@gmail.com>
2022-09-04 23:47:20 +0200

    l

---
+++ Functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Functions
+{
+    class MyMath
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calc_ggT(111, 123));
+            Console.ReadKey();
+        }
+
+
+        static int Calc_ggT(int a, int b)
+        {
+
+            while (b != 0)
+            {
+                int z = a % b;
+                a = b;
+                b = z;
+            }
+
+            return a;
+        }
+
+        static int Calc_kgV(int a, int b)
+        {
+           Calc_ggT(a, b) * Calc_kgV(a, b) = a * b;
+
+        }
+        
+    }
+}
