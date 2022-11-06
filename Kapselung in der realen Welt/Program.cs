625c8f6db9e3e1ba58ea60c312122f03565adefc
thi_nguyen <thi.nguyen@ksb-sg.ch>
2022-11-06 23:10:19 +0100

    Ok

---
+++ Kapselung in der realen Welt/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kapselung;
+
+public class Person
+{
+    public int wut  = 100;
+    { get; set; }
+    public int entspannt = 0;
+    { get; set; }
+}
---
+++ Triangle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Triangle;
+
+public class Triangle
+{
+    public string Name { get; set; }
+    public Color color
+    {
+        get; private set;
+    }
+
+    protected Point[3] Points
+    {
+        get; private set;
+    }
+
+    private Point center;
+
+    public int CalcCircumference;
+}
+
